d734eb0e7500083a34f10ff8768ad1bc8d47f5b4
BerganouY <yassineberganouu@gmail.com>
2025-07-27 13:15:08 +0100

    Add Django-Middleware-0x03

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'chats.middleware.RolePermissionMiddleware',
 ]
 
-ROOT_URLCONF = 'messaging_app.urls'
+ROOT_URLCONF = 'Django-Middleware-0x03.urls'
 
 TEMPLATES = [
     {
@@ -62,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'messaging_app.wsgi.application'
+WSGI_APPLICATION = 'Django-Middleware-0x03.wsgi.application'
 
 DATABASES = {
     'default': {

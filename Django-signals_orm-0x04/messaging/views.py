d59415652ebd7ab3178612c72cea5dba2f872657
BerganouY <yassineberganouu@gmail.com>
2025-08-04 21:20:30 +0100

    Add views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 User = get_user_model()
 
@@ -31,3 +33,22 @@ class DeleteUserView(APIView):
             {"detail": "User account and all related data deleted successfully."},
             status=status.HTTP_204_NO_CONTENT
     )
+
+
+class ThreadedMessagesView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        conversation_id = self.kwargs['conversation_id']
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+
+        # Optimized query with prefetch_related and select_related
+        return Message.objects.filter(
+            conversation=conversation
+        ).select_related(
+            'sender',
+            'parent_message',
+            'conversation'
+        ).prefetch_related(
+            'replies'
+        ).order_by('timestamp')
